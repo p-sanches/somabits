f7e43918318f172b04d9a23de0efc061ad32fa51
Martina Brachmann <martina.brachmann.uni@gmail.com>
2019-03-19 16:00:11 +0100

    Server is now sending the devices it has registered

--- PythonMDNSTest/TestServer_win.py
+++ PythonMDNSTest/TestServer_win.py
@@ -5,14 +5,70 @@
 import logging
 import sys
 import socket
+import os
 
 from typing import cast
 from time import sleep
-from zeroconf import ServiceBrowser, ServiceStateChange, Zeroconf
+from zeroconf import ServiceInfo, ServiceBrowser, ServiceStateChange, Zeroconf
 
-#TYPE = '_osc._udp.local.'
-TYPE = '_osc._udp.local.'
-NAME = 'Server'
+TYPE = "_osc._udp.local."
+NAME = ""
+
+
+def wait_key():
+    ''' Wait for a key press on the console and return it. '''
+    result = None
+    if os.name == 'nt':
+        import msvcrt
+        result = msvcrt.getch()
+    else:
+        import termios
+        #fd = sys.stdin.fileno()
+        fd = sys.stdin
+
+        oldterm = termios.tcgetattr(fd)
+        newattr = termios.tcgetattr(fd)
+        newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+        termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+        try:
+            result = sys.stdin.read(1)
+        except IOError:
+            pass
+        finally:
+            termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+
+    return result
+
+def simulate_save_button():
+    TXT_record = {'server': str(len(browser.services))}
+
+    # TODO: Check if a service has timed out. If so, unregister device.
+
+    for i, service in enumerate(browser.services.items()):
+        print(service)
+        record = str(service[1]).replace('[', ", ").replace(']', ", ").split(',')
+        record_name = record[len(record) - 2]
+
+        info  = zeroconf.get_service_info(TYPE, record_name)
+        if info:
+            TXT_record.update({'name' + str(i): info.server})
+        else:
+            print("Service type not found")
+
+    info = ServiceInfo(type_="_osc._udp.local.",
+                       name=NAME + "." + TYPE,
+                       address=socket.inet_aton("192.168.11.172"),
+                       port=80,
+                       weight=0,
+                       priority=0,
+                       properties=TXT_record,
+                       server=NAME + ".local.")
+
+    print("Registration of a service")
+    zeroconf.register_service(info)
+
+
 
 def on_service_state_change(
     zeroconf: Zeroconf, service_type: str, name: str, state_change: ServiceStateChange,) -> None:
@@ -37,17 +93,21 @@ def on_service_state_change(
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 2:
         assert sys.argv[1:] == ['--debug']
         logging.getLogger('zeroconf').setLevel(logging.DEBUG)
+    elif len(sys.argv) > 1:
+        NAME = sys.argv[1]
 
     zeroconf = Zeroconf()
     browser = ServiceBrowser(zeroconf, TYPE, handlers=[on_service_state_change])
 
     try:
         while True:
-            #print(zeroconf.get_service_info(TYPE, NAME + '.' + TYPE, timeout=30000))
-            sleep(0.1)
-
+            key = wait_key()
+            if key != None:
+                simulate_save_button()
+            else:
+                sleep(0.1)
     finally:
         zeroconf.close()
