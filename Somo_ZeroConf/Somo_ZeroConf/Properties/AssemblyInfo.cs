af20b49b3d97dac303bc87120ce31997ac9407c4
Naveed Bhatti <hunarame@gmail.com>
2019-03-19 09:24:57 +0100

    no message

--- Somo_ZeroConf/Somo_ZeroConf/Properties/AssemblyInfo.cs
+++ Somo_ZeroConf/Somo_ZeroConf/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("44164433-7890-9977-7890-444433534144")]
+[assembly: Guid("97374433-7890-3636-7890-444433529696")]
 
 // Version information for an assembly consists of the following four values:
 //
--- WifiMDNSTest/WifiMDNSTest.ino
+++ WifiMDNSTest/WifiMDNSTest.ino
@@ -80,7 +80,7 @@ void setup() {
   // Arduino via the host name "arduino.local", provided that your operating
   // system is mDNS/Bonjour-enabled (such as MacOS X).
   // Always call this before any other method!
-  mdns.begin(WiFi.localIP(), "arduino_1");
+  mdns.begin(WiFi.localIP(), "arduino_2");
 
   // Now let's register the service we're offering (a web service) via Bonjour!
   // To do so, we call the addServiceRecord() method. The first argument is the
@@ -126,7 +126,7 @@ void setup() {
 
   Serial.println(txt);
 
-  int success = mdns.addServiceRecord("Arduino with Accelerometer._http",
+  int success = mdns.addServiceRecord("Arduino with Accelerometer2._http",
                         3333,
                         MDNSServiceUDP,
                         txt);
