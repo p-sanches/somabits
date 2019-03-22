8208e9f1c3696c70455dbfe0b9ed4fbc1be4a980
Martina Brachmann <martina.brachmann.uni@gmail.com>
2019-03-22 19:30:12 +0100

    Fixed Things

--- PythonMDNSTest/Start.py
+++ PythonMDNSTest/Start.py
@@ -15,9 +15,14 @@ from time import sleep
 from zeroconf import ServiceInfo,ServiceBrowser, ServiceStateChange, Zeroconf
 from typing import List
 
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
 TYPE = '_osc._udp.local.'
 NAME = 'Server'
-Table_info = pd.DataFrame(columns=['Address', 'Port', 'Server','Select','Device Type','Device Address','Device Range'])
+#Table_info = pd.DataFrame(columns=['Address', 'Port', 'Server','Select','Device Type','Device Address','Device Range'])
+#TABLE_INFO = pd.DataFrame(columns=['Address', 'Port', 'Server','Select','Device Type','Device Address','Device Range'])
 Table_info_selected = pd.DataFrame(columns=['Address', 'Port', 'Server','Select','Device Type','Device Address','Device Range'])
 
 
@@ -27,41 +32,38 @@ class StartQT5(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.discover_button.clicked.connect(self.zeroconf_start)
-        #self.ui.save_button.clicked.connect(self.save_button_clicked)
+        self.TABLE_INFO = pd.DataFrame(columns=['Address', 'Port', 'Server','Select','Device Type','Device Address','Device Range'])
 
-    def close(self, app):
-        #self.zeroconf.close()
-        #self.neighbor_discovery
-        print("Closing App")
-        #self.neighbor_discovery.close()
-        #return app.exec_()
-
+        self.model = PandasModel(self.TABLE_INFO)
+        self.ui.tableView.setModel(self.model)
 
     def handleCheckboxClicked(self):
         Checkbox = QtWidgets.qApp.focusWidget()
         if Checkbox.isChecked():
-            self.discovery.register_service(Checkbox.accessibleName())
+            self.discovery.register_service(Checkbox.accessibleName(), Checkbox.accessibleDescription())
         else:
+            self.TABLE_INFO = self.TABLE_INFO[self.TABLE_INFO["Address"] != Checkbox.accessibleName()]
+            self.TABLE_INFO.index = self.TABLE_INFO.index + 1  # shifting index
+            self.TABLE_INFO = self.TABLE_INFO.sort_index()  # sorting by index
             self.discovery.unregister_service(Checkbox.accessibleName(), Checkbox.accessibleDescription())
 
-
     def on_device_found(self, zeroconf, service_type, name, state_change):
-        global Table_info
+        global TABLE_INFO
         update_tableView = True
+        device_type = []
+        device_address = []
+        device_range = []
+
         self.ui.plainTextEdit.appendPlainText(
             "Service %s of type %s state changed: %s" % (name, service_type, state_change))
 
         if state_change is ServiceStateChange.Added:
             info = zeroconf.get_service_info(service_type, name)
             if info:
-
                 self.ui.plainTextEdit.appendPlainText(
                     "  Address: %s:%d" % (socket.inet_ntoa(cast(bytes, info.address)), cast(int, info.port)))
                 self.ui.plainTextEdit.appendPlainText("  Weight: %d, priority: %d" % (info.weight, info.priority))
                 self.ui.plainTextEdit.appendPlainText("  Server: %s" % (info.server,))
-                device_type = []
-                device_address = []
-                device_range = []
 
                 if info.properties:
                     self.ui.plainTextEdit.appendPlainText("  Properties are:")
@@ -82,9 +84,33 @@ class StartQT5(QtWidgets.QMainWindow):
                 else:
                     print("  No properties")
 
-                if socket.inet_ntoa(cast(bytes, info.address)) not in Table_info.index:
-                    local_device = pd.DataFrame({'Address': socket.inet_ntoa(cast(bytes, info.address)), 'Port': cast(int, info.port),'Server': info.server, 'Select': "", 'Device Type': [device_type],'Device Address': [device_address],'Device Range': [device_range]}, index = [socket.inet_ntoa(cast(bytes, info.address))])
-                    Table_info = Table_info.append(local_device)
+                # FIXME: Just for debugging we show the server
+                #if (socket.inet_ntoa(cast(bytes, info.address)) not in TABLE_INFO.index): #or (info.server not in TABLE_INFO['Server']):
+                if (socket.inet_ntoa(cast(bytes, info.address)) not in self.TABLE_INFO["Address"].to_list()):
+                    #print(self.TABLE_INFO[self.TABLE_INFO.Address])
+                    #local_device = pd.DataFrame({'Address': socket.inet_ntoa(cast(bytes, info.address)), 'Port': cast(int, info.port),
+                    # 'Server': info.server, 'Select': "", 'Device Type': [device_type],'Device Address': [device_address],'Device Range': [device_range]}, index = [socket.inet_ntoa(cast(bytes, info.address))])
+                    #TABLE_INFO.ix[socket.inet_ntoa(cast(bytes, info.address))] = [socket.inet_ntoa(cast(bytes, info.address)), cast(int, info.port), info.server, False, device_type, device_address, device_range]
+                    self.TABLE_INFO.loc[len(self.TABLE_INFO)] = [
+                        socket.inet_ntoa(cast(bytes, info.address)), cast(int, info.port), info.server, False,
+                        device_type, device_address, device_range]
+                    #Table_info = Table_info.append(local_device)
+                    print(self.TABLE_INFO)
+                    #print(self.TABLE_INFO[self.TABLE_INFO.Address])
+                    self.Checkbox = QtWidgets.QCheckBox(' ')
+                    self.Checkbox.setAccessibleName(socket.inet_ntoa(cast(bytes, info.address)))
+                    self.Checkbox.setAccessibleDescription(name)
+                    self.Checkbox.clicked.connect(self.handleCheckboxClicked)
+
+                    checkBoxWidget = QtWidgets.QWidget()
+                    layoutCheckBox = QtWidgets.QHBoxLayout(checkBoxWidget)
+                    layoutCheckBox.addWidget(self.Checkbox)
+                    layoutCheckBox.setAlignment(Qt.AlignCenter);
+
+                    item = self.model.index(self.model.rowCount() - 1, 3)
+                    self.ui.tableView.setIndexWidget(item, self.Checkbox)
+
+                    self.model.insertRows()
                 else:
                     update_tableView = False
 
@@ -93,16 +119,26 @@ class StartQT5(QtWidgets.QMainWindow):
 
             self.ui.plainTextEdit.appendPlainText('\n')
 
-            if update_tableView:
-                model = PandasModel(Table_info)
-                self.ui.tableView.setModel(model)
-                for index in range(model.rowCount()):
-                    self.Checkbox = QtWidgets.QCheckBox(' ')
-                    self.Checkbox.setAccessibleName(socket.inet_ntoa(cast(bytes, info.address)))
-                    self.Checkbox.setAccessibleDescription(name)
-                    self.Checkbox.clicked.connect(self.handleCheckboxClicked)
-                    item = model.index(index, 3);
-                    self.ui.tableView.setIndexWidget(item, self.Checkbox)
+            #if update_tableView:
+            #    model = PandasModel(Table_info)
+            #    self.ui.tableView.setModel(model)
+            #    print(range(model.rowCount()))
+            #    #for index in range(model.rowCount()):
+            #    self.Checkbox = QtWidgets.QCheckBox(' ')
+            #    self.Checkbox.setAccessibleName(socket.inet_ntoa(cast(bytes, info.address)))
+            #    self.Checkbox.setAccessibleDescription(name)
+            #    self.Checkbox.clicked.connect(self.handleCheckboxClicked)
+            #    #item = model.index(index, 3);
+            #    item = model.index(model.rowCount() - 1, 3)
+            #    self.ui.tableView.setIndexWidget(item, self.Checkbox)
+            print(self.model.rowCount())
+            #print(self.model.index(self.model.rowCount(), 3))
+            #self.model.insertRow()
+            #print(self.model.index(self.model.rowCount(), 3))
+            #self.model.setData(self.model.index(self.model.rowCount(), 3), "")
+            #print(self.model.rowCount())
+        if state_change is ServiceStateChange.Removed:
+            print("Service Removed")
 
     def zeroconf_start(self):
         self.discovery = NeighborDiscovery()
@@ -120,24 +156,31 @@ class NeighborDiscovery(QtCore.QThread):
     def on_service_state_change(self,zeroconf: Zeroconf, service_type: str, name: str, state_change: ServiceStateChange, ) -> None:
         self.neighbor_signal.emit(zeroconf, service_type, name, state_change)
 
-    def register_service(self, ip):
+    def register_service(self, ip, service_name):
         TXT_record = {'server': self.get_local_ip()}
         TXT_record.update({"device": ip})
 
+        name = service_name.split('.')[0]
+        name = NAME + "_" + name
+
         info = ServiceInfo(type_="_osc._udp.local.",
-                           name=NAME + "." + TYPE,
+                           name=name + "." + TYPE,
                            address=socket.inet_aton("192.168.11.172"),
                            port=80,
                            weight=0,
                            priority=0,
                            properties=TXT_record,
-                           server=NAME + ".local.")
-
-        print("Registration of a service")
+                           server=name + ".local.")
+        print(info)
+        print("Registration of a service %s" % (name))
         self.zeroconf.register_service(info)
 
-    def unregister_service(self, ip, name):
+    def unregister_service(self, ip, name_):
+        print(name_)
+        name = NAME + "_" + name_
         info = self.zeroconf.get_service_info(TYPE, name)
+        print(info)
+        self.zeroconf.unregister_service(info)
         print("Unregistering service %s with IP %s" % (name, ip))
 
 
@@ -157,7 +200,6 @@ class NeighborDiscovery(QtCore.QThread):
 
 class PandasModel(QtCore.QAbstractTableModel):
     def __init__(self, df = pd.DataFrame(), parent=None):
-        #QtCore.QAbstractTableModel.__init__(self, parent=parent)
         QtCore.QAbstractTableModel.__init__(self)
         self._df = df
 
@@ -186,7 +228,8 @@ class PandasModel(QtCore.QAbstractTableModel):
 
         return QtCore.QVariant(str(self._df.ix[index.row(), index.column()]))
 
-    def setData(self, index, value, role):
+    def setData(self, index, value, role=QtCore.Qt.EditRole):
+        print(index.row())
         row = self._df.index[index.row()]
         col = self._df.columns[index.column()]
         if hasattr(value, 'toPyObject'):
@@ -212,6 +255,36 @@ class PandasModel(QtCore.QAbstractTableModel):
         self._df.sort_values(colname, ascending= order == QtCore.Qt.AscendingOrder, inplace=True)
         self._df.reset_index(inplace=True, drop=True)
         self.layoutChanged.emit()
+
+    # def removeRows(self, position, rows=1, index=QModelIndex()):
+    #     print("\n\t\t ...removeRows() Starting position: '%s'" % position, 'with the total rows to be deleted: ', rows)
+    #     self.beginRemoveRows(QModelIndex(), position, position + rows - 1)
+    #     self.items = self.items[:position] + self.items[position + rows:]
+    #     self.endRemoveRows()
+    #
+    #     return True
+
+    # def insertRows(self, position, rows=1, index=QModelIndex()):
+    #     print
+    #     "\n\t\t ...insertRows() Starting position: '%s'" % position, 'with the total rows to be inserted: ', rows
+    #     indexSelected = self.index(position, 0)
+    #     itemSelected = indexSelected.data().toPyObject()
+    #
+    #     self.beginInsertRows(QModelIndex(), position, position + rows - 1)
+    #     for row in range(rows):
+    #         self.items.insert(position + row, "%s_%s" % (itemSelected, self.added))
+    #         self.added += 1
+    #     self.endInsertRows()
+    #     return True
+
+    def insertRows(self):
+        #row = self._df.index[index.row()]
+        #column = 0
+        self.layoutAboutToBeChanged.emit()
+        self._df.append(self._df.tail(1))
+        self._df.reset_index(inplace=True, drop=True)
+        self.layoutChanged.emit()
+
 
 
 if __name__ == "__main__":
