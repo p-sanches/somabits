3c38d42d29a9b89831dfb3a4c438ea2e5c1a52ad
Martina Brachmann <martina.brachmann.uni@gmail.com>
2019-03-27 13:34:58 +0100

    Start.py is renamed to SomoApp and the classes NeighborDiscovery and PandasModel are outsourced to their own files

--- PythonMDNSTest/SomoApp.py
+++ PythonMDNSTest/SomoApp.py
@@ -5,10 +5,11 @@ import numpy as np
 import pandas as pd
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import (Qt, pyqtSignal)
-import ifaddr
 
 
-from gui import Ui_MainWindow
+from PythonMDNSTest.gui import Ui_MainWindow
+from PythonMDNSTest.TableModel import PandasModel
+from PythonMDNSTest.ZeroConf import NeighborDiscovery
 
 from typing import cast
 from zeroconf import ServiceInfo,ServiceBrowser, ServiceStateChange, Zeroconf
@@ -18,8 +19,6 @@ pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
-TYPE = '_osc._udp.local.'
-NAME = 'Server'
 
 
 class StartQT5(QtWidgets.QMainWindow):
@@ -242,131 +241,6 @@ class StartQT5(QtWidgets.QMainWindow):
     def zeroconf_start(self):
         self.discovery = NeighborDiscovery()
         self.discovery.neighbor_signal.connect(self.on_device_found)
-
-
-class NeighborDiscovery(QtCore.QThread):
-    neighbor_signal = QtCore.pyqtSignal(object, object, object, object)
-
-    def __init__(self):
-        QtCore.QThread.__init__(self)
-        self.zeroconf = Zeroconf()
-        self.browser = ServiceBrowser(self.zeroconf, TYPE, handlers=[self.on_service_state_change])
-
-    def on_service_state_change(self,zeroconf: Zeroconf, service_type: str, name: str, state_change: ServiceStateChange, ) -> None:
-        self.neighbor_signal.emit(zeroconf, service_type, name, state_change)
-
-    def register_service(self, ip, service_name):
-        TXT_record = {'server': self.get_local_ip()}
-        TXT_record.update({"device": ip})
-
-        name = service_name.split('.')[0]
-        name = NAME + "_" + name
-
-        info = ServiceInfo(type_=TYPE,
-                           name=name + "." + TYPE,
-                           address=socket.inet_aton(self.get_local_ip()),
-                           port=80,
-                           weight=0,
-                           priority=0,
-                           properties=TXT_record,
-                           server=name + ".local.")
-
-
-        self.zeroconf.register_service(info)
-        print("Registration of a service %s" % (name))
-
-    def unregister_service(self, ip, service_name):
-        name = NAME + "_" + service_name.split(".")[0] + "." + TYPE
-
-        info = self.zeroconf.get_service_info(TYPE, name)
-        if info:
-            self.zeroconf.unregister_service(info)
-            print("Unregistering service %s with IP %s" % (name, ip))
-
-
-    def get_all_addresses(self) -> List[str]:
-        return list(set(
-            addr.ip
-            for iface in ifaddr.get_adapters()
-            for addr in iface.ips
-            if addr.is_IPv4 and addr.network_prefix != 32  # Host only netmask 255.255.255.255
-        ))
-
-    def get_local_ip(self, starts_with="192"):
-        list_ip = self.get_all_addresses()
-        local_ip = [i for i in list_ip if i.startswith(starts_with)]
-        return local_ip[0]
-
-
-class PandasModel(QtCore.QAbstractTableModel):
-    def __init__(self, df = pd.DataFrame(), parent=None):
-        QtCore.QAbstractTableModel.__init__(self)
-        self._df = df
-
-    def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
-        if role != QtCore.Qt.DisplayRole:
-            return QtCore.QVariant()
-
-        if orientation == QtCore.Qt.Horizontal:
-            try:
-                return self._df.columns.tolist()[section]
-            except (IndexError, ):
-                return QtCore.QVariant()
-        elif orientation == QtCore.Qt.Vertical:
-            try:
-                # return self.df.index.tolist()
-                return self._df.index.tolist()[section]
-            except (IndexError, ):
-                return QtCore.QVariant()
-
-    def data(self, index, role=QtCore.Qt.DisplayRole):
-        if role != QtCore.Qt.DisplayRole:
-            return QtCore.QVariant()
-
-        if not index.isValid():
-            return QtCore.QVariant()
-
-        return QtCore.QVariant(str(self._df.ix[index.row(), index.column()]))
-
-    def setData(self, index, value, role=QtCore.Qt.EditRole):
-        print(index.row())
-        row = self._df.index[index.row()]
-        col = self._df.columns[index.column()]
-        if hasattr(value, 'toPyObject'):
-            # PyQt4 gets a QVariant
-            value = value.toPyObject()
-        else:
-            # PySide gets an unicode
-            dtype = self._df[col].dtype
-            if dtype != object:
-                value = None if value == '' else dtype.type(value)
-        self._df.set_value(row, col, value)
-        return True
-
-    def rowCount(self, parent=QtCore.QModelIndex()):
-        return len(self._df.index)
-
-    def columnCount(self, parent=QtCore.QModelIndex()):
-        return len(self._df.columns)
-
-    def sort(self, column, order):
-        colname = self._df.columns.tolist()[column]
-        self.layoutAboutToBeChanged.emit()
-        self._df.sort_values(colname, ascending= order == QtCore.Qt.AscendingOrder, inplace=True)
-        self._df.reset_index(inplace=True, drop=True)
-        self.layoutChanged.emit()
-
-    def insertRows(self):
-        self.layoutAboutToBeChanged.emit()
-        self._df.append(self._df.tail(1))
-        self._df.reset_index(inplace=True, drop=True)
-        self.layoutChanged.emit()
-
-    def removeRows(self, row):
-        self.layoutAboutToBeChanged.emit()
-        self._df = self._df[self._df['Address'] != row]
-        self._df.reset_index(inplace=True, drop=True)
-        self.layoutChanged.emit()
 
 
 if __name__ == "__main__":
