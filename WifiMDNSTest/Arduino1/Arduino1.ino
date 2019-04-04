93e6065176a3b0f5c016b3b94fb923b290b9b7ec
Martina Brachmann <martina.brachmann.uni@gmail.com>
2019-04-04 09:50:18 +0200

    Update .gitignore and Arduino1.ino

--- WifiMDNSTest/Arduino1/Arduino1.ino
+++ WifiMDNSTest/Arduino1/Arduino1.ino
@@ -37,6 +37,60 @@ void serviceFound(const char* type, MDNSServiceProtocol proto,
                   const char* name, IPAddress ip, unsigned short port,
                   const char* txtContent);
 
+void printMacAddress(byte mac[]) {
+  for (int i = 5; i >= 0; i--) {
+    if (mac[i] < 16) {
+      Serial.print("0");
+    }
+    Serial.print(mac[i], HEX);
+    if (i > 0) {
+      Serial.print(":");
+    }
+  }
+  Serial.println();
+}
+
+void printWifiData() {
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("Local IP Address: ");
+  Serial.println(ip);
+
+  // print your MAC address:
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  printMacAddress(mac);
+
+  IPAddress gateway_ip = WiFi.gatewayIP();
+  Serial.print("GateWay IP Address: ");
+  Serial.println(gateway_ip);
+
+}
+
+void printCurrentNet() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print the MAC address of the router you're attached to:
+  byte bssid[6];
+  WiFi.BSSID(bssid);
+  Serial.print("BSSID: ");
+  printMacAddress(bssid);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.println(rssi);
+
+  // print the encryption type:
+  byte encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type:");
+  Serial.println(encryption, HEX);
+  Serial.println();
+}
+
 void setup()
 {
   //Initialize serial and wait for port to open:
@@ -86,17 +140,18 @@ void setup()
   // named "serviceFound".
   mdns.setServiceFoundCallback(serviceFound);
 
-  Serial.println("Enter a mDNS service name via the Arduino Serial Monitor "
-                 "to discover instances");
-  Serial.println("on the network.");
-  Serial.println("Examples are \"_http\", \"_afpovertcp\" or \"_ssh\" (Note "
-                 "the underscores).");
+//  Serial.println("Enter a mDNS service name via the Arduino Serial Monitor "
+//                 "to discover instances");
+//  Serial.println("on the network.");
+//  Serial.println("Examples are \"_http\", \"_afpovertcp\" or \"_ssh\" (Note "
+//                 "the underscores).");
 }
 
 void loop()
 { 
-  char serviceName[256];
-  int length = 0;
+	//char serviceName[256] = "_osc\n";
+	char serviceName[256];
+	int length = 0;
   
   // read in a service name from the Arduino IDE's serial monitor.
   while (Serial.available()) {
@@ -110,7 +165,7 @@ void loop()
   // mDNS library is currently discovering service instances.
   // If so, we skip this input, since we want our previous request to continue.
   if (!mdns.isDiscoveringService()) {
-    if (length > 0) {    
+    if (length > 0) {
       Serial.print("Discovering services of type '");
       Serial.print(serviceName);
       Serial.println("' via Multi-Cast DNS (Bonjour)...");
@@ -126,8 +181,8 @@ void loop()
 
       mdns.startDiscoveringService(serviceName,
                                    MDNSServiceUDP,
-                                   0);
-    }  
+                                   9000);
+    }
   }
 
   // This actually runs the mDNS module. YOU HAVE TO CALL THIS PERIODICALLY,
@@ -186,58 +241,4 @@ void serviceFound(const char* type, MDNSServiceProtocol /*proto*/,
       }
     }
   }
-}
-
-void printWifiData() {
-  // print your board's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("Local IP Address: ");
-  Serial.println(ip);
-
-  // print your MAC address:
-  byte mac[6];
-  WiFi.macAddress(mac);
-  Serial.print("MAC address: ");
-  printMacAddress(mac);
-
-  IPAddress gateway_ip = WiFi.gatewayIP();
-  Serial.print("GateWay IP Address: ");
-  Serial.println(gateway_ip);
-
-}
-
-void printCurrentNet() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  // print the MAC address of the router you're attached to:
-  byte bssid[6];
-  WiFi.BSSID(bssid);
-  Serial.print("BSSID: ");
-  printMacAddress(bssid);
-
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.println(rssi);
-
-  // print the encryption type:
-  byte encryption = WiFi.encryptionType();
-  Serial.print("Encryption Type:");
-  Serial.println(encryption, HEX);
-  Serial.println();
-}
-
-void printMacAddress(byte mac[]) {
-  for (int i = 5; i >= 0; i--) {
-    if (mac[i] < 16) {
-      Serial.print("0");
-    }
-    Serial.print(mac[i], HEX);
-    if (i > 0) {
-      Serial.print(":");
-    }
-  }
-  Serial.println();
 }
