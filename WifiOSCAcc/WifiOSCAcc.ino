8d623b2527ffd96fcd2dbd5acf6dba507c8beb08
Martina Brachmann <martina.brachmann.uni@gmail.com>
2019-04-08 13:41:28 +0200

    Merge branch 'master' of https://MartinaBr@bitbucket.org/somapixel/somaserver.git
    And cleanup

--- WifiOSCAcc/WifiOSCAcc.ino
+++ WifiOSCAcc/WifiOSCAcc.ino
@@ -32,16 +32,15 @@ OSCErrorCode error;
 unsigned int ledState = LOW;
 
 // A UDP instance to let us send and receive packets over UDP
-WiFiUDP udp;
-MDNS mdns(udp);
+WiFiUDP udp_mdsn;
+WiFiUDP udp_osc;
+MDNS mdns(udp_mdsn);
 const unsigned int tcp_port = 5555;
 WiFiServer server(tcp_port);
 WiFiClient client;
 
-//IPAddress server_ip(192, 168, 11, 103); //change it to your server IP address
 IPAddress broadcast_ip(0, 0, 0, 0);
 const unsigned int server_port = 3333;
-const unsigned int local_port = 3333;
 
 LSM6DS3 myIMU(SPI_MODE, SPIIMU_SS); // SPI Chip Select
 
@@ -102,13 +101,6 @@ void printCurrentNet() {
 	Serial.println();
 }
 
-//void led(OSCMessage &msg) {
-//	ledState = msg.getInt(0);
-//	digitalWrite(LED_BUILTIN, ledState);
-//	Serial.print("/led: ");
-//	Serial.println(ledState);
-//}
-
 void setup() {
 	//Initialize serial and wait for port to open:
 	Serial.begin(9600);
@@ -149,7 +141,7 @@ void setup() {
 	server.begin();
 
 	Serial.println("\nStarting connection to server...");
-	udp.begin(local_port);
+	udp_osc.begin(server_port);
 
 	Serial.println("\nStarting Service Discovery...");
 	mdns.begin(WiFi.localIP(), "arduino_acc");
@@ -170,7 +162,7 @@ void setup() {
 	}
 
 	int success;
-	success = mdns.addServiceRecord("Arduino with Accelerometer_1._osc", 3333,
+	success = mdns.addServiceRecord("Arduino with Accelerometer._osc", 3333,
 			MDNSServiceUDP, txt);
 
 	if (success) {
@@ -192,7 +184,6 @@ void loop() {
 		while (client.connected()) {        // loop while the client's connected
 			if (client.available()) { // if there's bytes to read from the client
 				Serial.println(client.remoteIP());
-//				Serial.println(client.remotePort());
 				if (client.remoteIP() != broadcast_ip) {
 					char c = NULL;
 					do {
@@ -222,103 +213,16 @@ void loop() {
 
 	if (server_ready == true) {
 
-//		OSCMessage send_msg("/accelerometer");
-//		OSCMessage recieve_msg("/accelerometer");
-
-//		Serial.print("Sending packet to ");
-//		Serial.print(server_ip);
-//		Serial.print(":");
-//		Serial.println(server_port);
-
 		send_msg.add(myIMU.readFloatAccelX());
 		Serial.println(myIMU.readFloatAccelX());
 
-		udp.beginPacket(server_ip, server_port);
-		send_msg.send(udp); // send the bytes to the SLIP stream
-		udp.endPacket(); // mark the end of the OSC Packet
+		udp_osc.beginPacket(server_ip, server_port);
+		send_msg.send(udp_osc); // send the bytes to the SLIP stream
+		udp_osc.endPacket(); // mark the end of the OSC Packet
 		send_msg.empty(); // free space occupied by message
-
-		//// Recieving ////
-
-		//	int size = udp.parsePacket();
-
-		//	if (size > 0) {
-		//
-		//		while (size--) {
-		//			recieve_msg.fill(udp.read());
-		//		}
-		//
-		//		if (!recieve_msg.hasError()) {
-		//			recieve_msg.dispatch("/led", led);
-		//		} else {
-		//			error = recieve_msg.getError();
-		//			Serial.print("error: ");
-		//			Serial.println(error);
-		//		}
-		//	}
-
 	}
 
 	mdns.run();
 
 	delay(10);
 }
-
-//void nameFound(const char* name, IPAddress ip)
-//{
-//  if (ip != INADDR_NONE) {
-//    Serial.print("The IP address for '");
-//    Serial.print(name);
-//    Serial.print("' is ");
-//    Serial.println(ip);
-//  } else {
-//    Serial.print("Resolving '");
-//    Serial.print(name);
-//    Serial.println("' timed out.");
-//  }
-//}
-
-//void serviceFound(const char* type, MDNSServiceProtocol /*proto*/,
-//                  const char* name, IPAddress ip,
-//                  unsigned short port,
-//                  const char* txtContent)
-//{
-//  if (NULL == name) {
-//    Serial.print("Finished discovering services of type ");
-//    Serial.println(type);
-//  } else {
-//    Serial.print("Found: '");
-//    Serial.print(name);
-//    Serial.print("' at ");
-//    Serial.print(ip);
-//    Serial.print(", port ");
-//    Serial.print(port);
-//    Serial.println(" (UDP)");
-//
-//    // Check out http://www.zeroconf.org/Rendezvous/txtrecords.html for a
-//    // primer on the structure of TXT records. Note that the Bonjour
-//    // library will always return the txt content as a zero-terminated
-//    // string, even if the specification does not require this.
-//    if (txtContent) {
-//      Serial.print("\ttxt record: ");
-//
-//      char buf[256];
-//      char len = *txtContent++;
-//      int i = 0;
-//      while (len) {
-//        i = 0;
-//        while (len--)
-//          buf[i++] = *txtContent++;
-//        buf[i] = '\0';
-//        Serial.print(buf);
-//        len = *txtContent++;
-//
-//        if (len)
-//          Serial.print(", ");
-//        else
-//          Serial.println();
-//      }
-//    }
-//  }
-//}
-
